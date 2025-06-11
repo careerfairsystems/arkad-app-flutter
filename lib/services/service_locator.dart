08b304c6a277889eb2a20eeb5beeb694cfc70b50
Hanxuan Lin <h289lin@uwaterloo.ca>
2025-06-11 18:25:23 +0200

    Resolve all compilation errors

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -7,22 +7,20 @@ import 'package:arkad_api/arkad_api.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+
 final GetIt serviceLocator = GetIt.instance;
 
 /// Centrialized initialization of all the services and providers. An alternative to Flutter’s inherited widgets and provides a more decoupled way to access services and providers throughout the app. This abstracts the complexity from lower-level components and avoids the need to pass dependencies down the widget tree.
 
 // We could have used InheritedWidget or riverpod package to handle state, but GetIt with provider is a solid combo to my understanding.
 void setupServiceLocator() {
+  serviceLocator.registerLazySingleton<ArkadApi>(
+    () => ArkadApi(basePathOverride: 'https://staging.backend.arkadtlth.se'),
+  );
   serviceLocator.registerLazySingleton<FlutterSecureStorage>(
     () => const FlutterSecureStorage(),
   );
   serviceLocator.registerLazySingleton<http.Client>(() => http.Client());
-
-  serviceLocator.registerLazySingleton<ArkadApi>(
-    () => ArkadApi(basePathOverride: 'https://staging.backend.arkadtlth.se'),
-  );
-
-  serviceLocator.registerSingleton(AuthModel);
 
   serviceLocator.registerLazySingleton<ThemeModel>(() => ThemeModel());
   serviceLocator.registerLazySingleton<CompanyService>(() => CompanyService());
@@ -31,4 +29,5 @@ void setupServiceLocator() {
   serviceLocator.registerLazySingleton<StudentSessionModel>(
     () => StudentSessionModel(),
   );
+  serviceLocator.registerSingleton(AuthModel());
 }
