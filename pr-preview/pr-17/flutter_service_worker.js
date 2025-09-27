89e48029045855fea7efe7c4e8e10e8ffbc210cf
Hugo-Persson <Hugo-Persson@users.noreply.github.com>
2025-09-27 05:38:08 +0000

    Deploy preview for PR 17 🛫

--- pr-preview/pr-17/flutter_service_worker.js
+++ pr-preview/pr-17/flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "1f22339dfe208aea086dd8637dead7e0",
+"flutter_bootstrap.js": "1e312057cb7d8a89d57927fb4905067e",
 "version.json": "aafd9b171b7bba9f530bcd216fdc4885",
-"main.dart.js": "317efe0689047053899e717b714cde68"};
+"main.dart.js": "8327a87965b5194a80d2fb7df83ac13c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
