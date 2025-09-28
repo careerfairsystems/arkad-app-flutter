7c094d8c6443416e4f8383d984d058b0e4478ca8
Hugo-Persson <Hugo-Persson@users.noreply.github.com>
2025-09-28 06:27:34 +0000

    Deploy preview for PR 17 🛫

--- pr-preview/pr-17/flutter_bootstrap.js
+++ pr-preview/pr-17/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1728064415"
+    serviceWorkerVersion: "3584926231"
   }
 });
