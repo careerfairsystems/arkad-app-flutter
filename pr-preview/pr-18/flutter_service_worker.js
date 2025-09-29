af83a24b28757810128edf30d161009944054640
hanxuan-lin <hanxuan-lin@users.noreply.github.com>
2025-09-29 13:05:58 +0000

    Deploy preview for PR 18 🛫

--- pr-preview/pr-18/flutter_service_worker.js
+++ pr-preview/pr-18/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "258e0bfb028fc552c0b21aad37976692",
+"flutter_bootstrap.js": "4b9a24f9a653127ac2487f23ca2b7db3",
 "version.json": "aafd9b171b7bba9f530bcd216fdc4885",
 "main.dart.js": "75b98bc7c2cac832071caa64d0dc1f09"};
 // The application shell files that are downloaded before a service worker can
