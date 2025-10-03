46b1f3f27cfe86b94a568718fe364d7dd548ebe0
Hugo-Persson <hugo.e.persson@gmail.com>
2025-10-03 16:13:57 +0000

    Deploying to gh-pages from @ careerfairsystems/arkad-app-flutter@b434dbddd0286e74c67143a9a9d03fa6f5fd266f 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "9be6ace039d420c75bb7bd541ac4ede5",
+"flutter_bootstrap.js": "62f4b2fb54713f39ec2dccdc0f19beab",
 "version.json": "aafd9b171b7bba9f530bcd216fdc4885",
 "main.dart.js": "1c7835a42b557b3d8a00b6a828b9f327"};
 // The application shell files that are downloaded before a service worker can
