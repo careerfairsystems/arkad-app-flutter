83f6362c435f14da52f8fff3322adb8690ca9573
Hugo-Persson <Hugo-Persson@users.noreply.github.com>
2025-10-04 13:53:47 +0000

    Deploy preview for PR 20 🛫

--- pr-preview/pr-20/flutter_bootstrap.js
+++ pr-preview/pr-20/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "595302817"
+    serviceWorkerVersion: "3067558344"
   }
 });
--- pr-preview/pr-20/flutter_service_worker.js
+++ pr-preview/pr-20/flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "099415fd17fda2dd050cd4bfc007d4af",
+"flutter_bootstrap.js": "b1b04753595aad624d8a44732c13af25",
 "version.json": "aafd9b171b7bba9f530bcd216fdc4885",
-"main.dart.js": "12b3e11fbdffc47bcf1dee692c9fdaa8"};
+"main.dart.js": "dc764525acd6a9d84fdf62e938984854"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
