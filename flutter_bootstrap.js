15325fffd2292f7db327d95adab7baeb0b1a2bcf
Hugo-Persson <hugo.e.persson@gmail.com>
2025-10-05 14:53:31 +0000

    Deploying to gh-pages from @ careerfairsystems/arkad-app-flutter@6b8af7ecba3211ff67e00dfac56499e1a7719438 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4120760178"
+    serviceWorkerVersion: "975770074"
   }
 });
