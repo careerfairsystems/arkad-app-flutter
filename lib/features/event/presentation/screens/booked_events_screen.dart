65b7718f22aab0b5b3373b40142c1d767b72711f
Axel Hallefält <axelhallefalt@protonmail.com>
2025-10-08 19:53:44 +0200

    analyze

--- lib/features/event/presentation/screens/booked_events_screen.dart
+++ lib/features/event/presentation/screens/booked_events_screen.dart
@@ -1,4 +1,3 @@
-import 'package:arkad/features/event/presentation/screens/event_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
